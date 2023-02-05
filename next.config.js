eb6d6f96fabded229fe7409ecc9f27fef5fdf540
Matija Salopek <matija.salopek994@gmail.com>
2023-02-05 13:03:28 +0100

    add IBC docs from repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  unstable_staticImage: true,
+  staticImage: true,
 })
 
 module.exports = withNextra()
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,73 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Replicated Security Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/cosmos/interchain-security',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/cosmos/interchain-security/tree/main/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Replicated Security Docs',
   },
+  useNextSeoProps() {
+    const { frontMatter } = useConfig()
+    return {
+      // additionalLinkTags: [
+      //   {
+      //     href: '/apple-icon-180x180.png',
+      //     rel: 'apple-touch-icon',
+      //     sizes: '180x180'
+      //   },
+      //   {
+      //     href: '/android-icon-192x192.png',
+      //     rel: 'icon',
+      //     sizes: '192x192',
+      //     type: 'image/png'
+      //   },
+      //   {
+      //     href: '/favicon-96x96.png',
+      //     rel: 'icon',
+      //     sizes: '96x96',
+      //     type: 'image/png'
+      //   },
+      //   {
+      //     href: '/favicon-32x32.png',
+      //     rel: 'icon',
+      //     sizes: '32x32',
+      //     type: 'image/png'
+      //   },
+      //   {
+      //     href: '/favicon-16x16.png',
+      //     rel: 'icon',
+      //     sizes: '16x16',
+      //     type: 'image/png'
+      //   }
+      // ],
+      additionalMetaTags: [
+        { content: 'en', httpEquiv: 'Content-Language' },
+        { content: 'Nextra', name: 'apple-mobile-web-app-title' },
+        { content: '#fff', name: 'msapplication-TileColor' },
+        { content: '/ms-icon-144x144.png', name: 'msapplication-TileImage' }
+      ],
+      description:
+        frontMatter.description || 'ICS Docs: Replicated Security docs page for Cosmos SDK based chains',
+      // openGraph: {
+      //   images: [
+      //     { url: frontMatter.image || 'https://nextra.vercel.app/og.png' }
+      //   ]
+      // },
+      titleTemplate: '%s – Replicated Security Docs',
+      // twitter: {
+      // cardType: 'summary_large_image',
+      // site: 'https://nextra.vercel.app'
+      // }
+    }
+  }
 }
 
 export default config
